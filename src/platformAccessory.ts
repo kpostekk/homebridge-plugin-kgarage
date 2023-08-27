6d4fbfe5cd5740bdae7ddde1899609e38a82d724
Krystian Postek <krystian@postek.eu>
2023-08-27 23:06:38 +0200

    fix: resolve minor issues

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,6 +1,6 @@
-import { Service, PlatformAccessory, CharacteristicValue } from "homebridge";
-import { ControlTCPClient } from "kgarage-ctl/dist/tcp";
-import { ExampleHomebridgePlatform } from "./platform";
+import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
+import { ControlTCPClient } from 'kgarage-ctl/dist/tcp';
+import { ExampleHomebridgePlatform } from './platform';
 
 // const s= Characteristic.CurrentDoorState
 
@@ -17,7 +17,7 @@ export class KGarageDoorPlatformAccessory {
 
   constructor(
     private readonly platform: ExampleHomebridgePlatform,
-    private readonly accessory: PlatformAccessory
+    private readonly accessory: PlatformAccessory,
   ) {
     this.state = {
       current: this.platform.Characteristic.CurrentDoorState.CLOSED,
@@ -29,12 +29,12 @@ export class KGarageDoorPlatformAccessory {
       .getService(this.platform.Service.AccessoryInformation)!
       .setCharacteristic(
         this.platform.Characteristic.Manufacturer,
-        "Postek Company"
+        'Postek Company',
       )
-      .setCharacteristic(this.platform.Characteristic.Model, "GD:prototype:1")
+      .setCharacteristic(this.platform.Characteristic.Model, 'GD:prototype:1')
       .setCharacteristic(
         this.platform.Characteristic.SerialNumber,
-        "00-00-00-000000"
+        '00-00-00-000000',
       );
 
     this.service =
@@ -43,7 +43,7 @@ export class KGarageDoorPlatformAccessory {
 
     this.service.setCharacteristic(
       this.platform.Characteristic.Name,
-      accessory.context.device.name
+      accessory.context.device.name,
     );
 
     // required characteristics: Current Door State, Target Door State, Obstruction Detected
@@ -61,40 +61,42 @@ export class KGarageDoorPlatformAccessory {
 
     this.service.setCharacteristic(
       this.platform.Characteristic.CurrentDoorState,
-      this.state.current
+      this.state.current,
     );
     this.service.setCharacteristic(
       this.platform.Characteristic.TargetDoorState,
-      this.state.target
+      this.state.target,
     );
 
     this.client = new ControlTCPClient(accessory.context.device.secret);
     this.client.setTimeout(10_000);
     this.client.connect(
       accessory.context.device.port,
-      accessory.context.device.address
+      accessory.context.device.address,
     );
     setInterval(() => {
-      if (this.client?.readable) return;
+      if (this.client?.readable) {
+        return;
+      }
       this.platform.log.warn(
-        "Reconnecting to " + accessory.context.device.name
+        'Reconnecting to ' + accessory.context.device.name,
       );
       this.client?.connect(
         accessory.context.device.port,
-        accessory.context.device.address
+        accessory.context.device.address,
       );
     }, 12_000);
 
-    this.client.on("data", (data) => {
+    this.client.on('data', (data) => {
       const state = Number(data);
       this.state.current = state;
       this.service.setCharacteristic(
         this.platform.Characteristic.CurrentDoorState,
-        state
+        state,
       );
     });
 
-    this.client.on("error", (error) => {
+    this.client.on('error', (error) => {
       this.platform.log.error(String(error));
     });
   }
@@ -105,7 +107,7 @@ export class KGarageDoorPlatformAccessory {
 
   async setTargetDoorState(value: CharacteristicValue) {
     this.client?.sendControlPacket({
-      action: "SET",
+      action: 'SET',
       target: value as 0 | 1,
       timestamp: Date.now(),
     });
